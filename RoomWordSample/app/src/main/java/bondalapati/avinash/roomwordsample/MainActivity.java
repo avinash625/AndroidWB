9387587d3e0c41d61c62ecd805db0c68299fc828
Avinash Bondalapati <avinash.bondalapati@kony.com>
2019-05-17 14:33:26 +0530

    initial android wb commit

--- RoomWordSample/app/src/main/java/bondalapati/avinash/roomwordsample/MainActivity.java
+++ RoomWordSample/app/src/main/java/bondalapati/avinash/roomwordsample/MainActivity.java
@@ -1,5 +1,7 @@
 package bondalapati.avinash.roomwordsample;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,14 +9,25 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.RoomDatabase;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WordViewModel mWordViewModel;
+    private int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +45,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
+        mWordViewModel.getAllWords().observe(this,new Observer<List<Word>>(){
+
+            @Override
+            public void onChanged(List<Word> words) {
+                adapter.setWords(words);
             }
         });
     }
@@ -55,8 +78,36 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id == R.id.action_deleteall){
+            new AsyncTask<Void,Void,Void>(){
+
+                @Override
+                protected Void doInBackground(Void... voids) {
+                    WordRoomDatabase db = WordRoomDatabase.getDatabase(getApplicationContext());
+                    db.WordDao().deleteAll();
+                    return null;
+                }
+            }.execute();
+            return true;
+        }else if(id == R.id.app_bar_search){
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
--- RoomWordSample/app/src/main/java/bondalapati/avinash/roomwordsample/WordRoomDatabase.java
+++ RoomWordSample/app/src/main/java/bondalapati/avinash/roomwordsample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package bondalapati.avinash.roomwordsample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Word.class, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -15,12 +18,43 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
     public static WordRoomDatabase getDatabase(final Context context){
         if(INSTANCE == null){
-            Room.databaseBuilder(context.getApplicationContext(),
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                     WordRoomDatabase.class, "word_database")
                     //comment the following line of code to validate if it will cause any errors.
                     .fallbackToDestructiveMigration()
+                    //adding a callback for the populating the db on open.
+                    .addCallback(sRoomDatabaseCallback)
                     .build();
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDBAsync(INSTANCE).execute();
+        }
+
+    };
+
+    private static class PopulateDBAsync extends AsyncTask<Void,Void,Void>{
+        private  final WordDao mDao;
+
+        private PopulateDBAsync(WordRoomDatabase db) {
+            mDao = db.WordDao();
+        }
+
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+            Word word = new Word("Avinash");
+            mDao.insert(word);
+            word = new Word("Avinash 1");
+            mDao.insert(word);
+            return null;
+        }
+
     }
 }
